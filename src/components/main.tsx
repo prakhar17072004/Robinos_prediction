1602d1b45805cee78176cbdc416559831b651f69
prakhar17072004 <pmankur2004@gmail.com>
2024-12-19 00:14:34 +0530

    buttons are  updated

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function main() {
   return (
-    <div className=' mt-[10px] ml-[800px]'>
+    <div className=' mt-[10px] ml-[850px]'>
         <div className="flex">
-            <div className="rounded-[10px] font-medium text-14 relative border-2 border-gradient bg-slate-900 hidden md:flex mr-[10px]">
-                <div className="flex items-center h-[50px] p-[5px]">
-                    <button type="button" role="combobox" aria-controls="radix-:Rltff6la:" aria-expanded="false" aria-autocomplete="none" dir="ltr" data-state="closed" className="group h-full items-center rounded-[5px] border-2 border-darkblue-lighter placeholder:text-white text-white outline-none disabled:cursor-not-allowed disabled:opacity-50 [&amp;>span]:line-clamp-1 z-50 flex rounded-r-[10px] px-[15px] mx-[5px] min-w-48 justify-center border-none">
+            <div className="rounded-[10px] font-medium text-14 relative  gradient  hidden md:flex mr-[10px]">
+                <div className="flex items-center h-[50px] p-[2px] ">
+                    <button type="button"  className="group h-full items-center rounded-[10px] bg-slate-900 border-2 border-blue-500 placeholder:text-white text-white outline-none disabled:cursor-not-allowed disabled:opacity-50 [&amp;>span]:line-clamp-1 z-50 flex rounded-r-[10px] px-[15px]  min-w-48 justify-center border-none">
                         <span ><div className="flex items-center "><div className="flex items-center px-[10px]">
                         <div className="">Telos</div>
                         </div>
@@ -15,7 +15,7 @@ function main() {
                         </button>
                         </div>
                         </div>
-                        <div className="rounded-[10px] font-medium text-[16px] px-[20px] relative bg-blue-500 hover-bg-blue-400 transition cursor-pointer">
+                        <div className="rounded-[10px] font-medium text-[16px] px-[20px] relative bg-blue-500 hover:bg-blue-400 transition cursor-pointer">
             <div className="flex items-center justify-center h-[50px]"><span>Connect wallet</span></div></div></div>
     </div>
   )

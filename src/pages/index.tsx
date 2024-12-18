15a30ae653395d337220551cd3b97482c0658fc7
prakhar17072004 <pmankur2004@gmail.com>
2024-12-18 18:53:40 +0530

    sidebar is completed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Sidebar from '@/components/sidebar';
 import "../app/globals.css"
 
 const Index: React.FC = () => {
-  return <div className='bg-red-400 h-[100vh]'>
+  return <div className='h-[100vh]'>
     
 
 <div className=''><Sidebar/></div>
